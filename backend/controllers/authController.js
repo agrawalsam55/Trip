b9dc71321c0ff1255db7a4a895d19c18df2599c7
agrawalsam55 <135312926+agrawalsam55@users.noreply.github.com>
2025-07-04 00:47:43 +0530

    Create authController.js

---
+++ backend/controllers/authController.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+import { jwtSecret } from '../config.js';
+
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  const hashed = await bcrypt.hash(password, 10);
+  const user = new User({ name, email, password: hashed });
+  await user.save();
+  res.status(201).json({ message: 'User registered' });
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user || !(await bcrypt.compare(password, user.password)))
+    return res.status(401).json({ message: 'Invalid credentials' });
+
+  const token = jwt.sign({ id: user._id }, jwtSecret, { expiresIn: '1d' });
+  res.json({ token });
+};
