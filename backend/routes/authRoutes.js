a5a6c95602d867f912816e7813827691b03b9cbb
agrawalsam55 <135312926+agrawalsam55@users.noreply.github.com>
2025-07-05 01:18:47 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
-import express from 'express';
-import { signup, login } from '../controllers/authController.js';
+const express = require('express');
+const { registerUser, loginUser, logoutUser } = require('../controllers/authController');
+
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/login', login);
+router.post('/signup', registerUser);
+router.post('/login', loginUser);
+router.post('/logout', logoutUser);
 
-export default router;
+module.exports = router;
