999a337cac5e8622ac5e2b215bc592a887185091
agrawalsam55 <135312926+agrawalsam55@users.noreply.github.com>
2025-07-05 01:19:13 +0530

    Update tripRoutes.js

--- backend/routes/tripRoutes.js
+++ backend/routes/tripRoutes.js
@@ -1,12 +1,13 @@
-import express from 'express';
-import { authenticate } from '../middleware/authMiddleware.js';
-import { createTrip, getTrips, updateTrip, deleteTrip } from '../controllers/tripController.js';
-const router = express.Router();
+const express = require('express');
+const { authenticate } = require('../middleware/authMiddleware');
+const { createTrip, getTrips, updateTrip, deleteTrip } = require('../controllers/tripController');
 
+const router = express.Router();
 router.use(authenticate);
+
 router.post('/', createTrip);
 router.get('/', getTrips);
 router.put('/:id', updateTrip);
 router.delete('/:id', deleteTrip);
 
-export default router;
+module.exports = router;
