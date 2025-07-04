f469e0957f0715ec1d6a800557e63575daf2ad07
agrawalsam55 <135312926+agrawalsam55@users.noreply.github.com>
2025-07-05 01:17:14 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,27 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  fullname: {
+    firstname: { type: String, required: true, minlength: 3 },
+    lastname: { type: String, minlength: 3 }
+  },
+  email: { type: String, required: true, unique: true, minlength: 5 },
+  password: { type: String, required: true, select: false },
+  socketId: { type: String }
 });
 
-export default mongoose.model('User', userSchema);
+userSchema.methods.generateAuthToken = function () {
+  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+};
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashedPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+module.exports = mongoose.model('User', userSchema);
